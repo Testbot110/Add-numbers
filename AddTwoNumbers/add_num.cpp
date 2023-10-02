b8e76c5836dd7481e6ff001954ac6227b169376a
Avinash Krishna <krishnaavinash98@gmail.com>
2023-10-02 15:26:08 +0530

    Added program to find the sum of two numbers in C++, Java and Python.

---
+++ AddTwoNumbers/add_num.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    cout<<"Enter two numbers: "<<endl;
+
+    //Declaring variable and taking user input
+    float num1, num2;
+    cin>>num1>>num2;
+    
+    //Storing the sum of two numbers
+    float sum=num1+num2;
+    
+    //Printing output
+    cout<<"Sum of the two numbers is: "<<sum<<endl;
+
+    return 0;
+}
